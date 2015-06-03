ad928e69585ee53180d8990d34592a1be7fe8d27
Wen Zhang <wen@click3x.com>
2015-06-03 17:56:32 -0400

    family history page content and style

--- wp-content/themes/wen-theme/page-about.php
+++ wp-content/themes/wen-theme/page-about.php
@@ -21,60 +21,55 @@
 
 				<div id="inner-content" class="wrap cf">
 
-						<main id="main" class="m-all cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-							<div class="sub-title"><?php echo $sub_title; ?></div>
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<main id="main" class="m-all cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+<!-- 
+						<article id="featured-posts" class="cs featured-posts" role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-							
+							<header class="article-header">
+									<h2 class="page-sub-title italic">Featured</h2>
+									<h1 class="page-title">Posts</h1>
+							</header>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+							<section id="projects-list" class="projects-list cf" itemprop="articleBody">
+								<?php printProject($projects); ?>
+							</section>
 
-								<header class="article-header">
-
-									<h1 class="page-title"><?php the_title(); ?></h1>
-
-								</header>
-
-								<section class="entry-content cf" itemprop="articleBody">
-									<?php
-										// the content (pretty self explanatory huh)
-										the_content();
-
-										/*
-										 * Link Pages is used in case you have posts that are set to break into
-										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-										 *
-										*/
-										wp_link_pages( array(
-											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-											'after'       => '</div>',
-											'link_before' => '<span>',
-											'link_after'  => '</span>',
-										) );
-									?>
-								</section>
+						</article> -->
+						<!-- <div class="sub-title"><?php echo $sub_title; ?></div> -->
+						<div class="tabs-container">
+							<a class="btn history-tab">WHO? WHAT?</a>
+							<a class="btn history-tab">CHRONOLOGY</a>
+						</div>
 
 
-								<footer class="article-footer">
+						<?php
 
-								</footer>
+						// check if the repeater field has rows of data
+						if( have_rows('who_what') ):
 
-							</article>
+						 	// loop through the rows of data
+						    while ( have_rows('who_what') ) : the_row();
 
-							<?php endwhile; else : ?>
+						        // display a sub field value
+						        the_sub_field('headline'); ?>
+						        <div class="dashed-border"></div>
 
-							<?php endif; ?>
+						        <div class="copy-container">
+						        <?php the_sub_field('copy'); ?>
+						        </div>
 
-						</main>
+						    <?php endwhile;
 
-						<?php //  get_sidebar(); ?>
+						else :
+
+						    // no rows found
+
+						endif; ?>
+
+
+
+
+					</main>
 
 				</div>
 
