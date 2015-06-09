dce9f418cf690b26bd8de292d338742729574de3
Charlie Davis <charliekuldip@gmail.com>
2015-06-09 15:46:33 -0400

    Added smooth scroll to in-page links

--- wp-content/themes/wen-theme/library/js/scripts.js
+++ wp-content/themes/wen-theme/library/js/scripts.js
@@ -227,6 +227,20 @@ jQuery(document).ready(function($) {
 	});
 	// END SLIDERS
 
-	
+	// SMOOTH SCROLL TO IN-PAGE LINKS
+	$('a[href*=#]:not([href=#])').on('click touchstart', function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$("html, body").animate({
+					scrollTop: target.offset().top - 30
+				}, 1000);
+				return false;
+			}
+		}
+	});
+	// END SMOOTH SCROLL
+
 
 }); 
