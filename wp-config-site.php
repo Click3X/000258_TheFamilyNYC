2456814a3a02d3bc4719216b0a475debd38fae00
Charlie Davis <charliekuldip@gmail.com>
2015-06-24 15:04:10 -0400

    Added new footer

---
+++ wp-config-site.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, and ABSPATH. You can find more information by visiting
+ * {@link https://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
+ * Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'thefamily15');
+
+/** MySQL database username */
+define('DB_USER', 'thefamily15');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'CeDrEph5!');
+
+/** MySQL hostname */
+// define('DB_HOST', 'familynycwp.db.7573293.hostedresource.com');
+define('DB_HOST', 'thefamily15.db.7573293.hostedresource.com');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+
+define('AUTH_KEY',         'oo@4+~a.]SD*)+)wqI|YGl!5;XbLjFidJn(IZ,||Cv)JXm${!LZ+F-=Txy/V$d-K');
+define('SECURE_AUTH_KEY',  '5nc +O6|~ G_x` d2gb *L>qPunqluK$KjFG{/GkXA<,eGhT &uG%&){7hD_3 t+');
+define('LOGGED_IN_KEY',    '5&Xb{k7a-JM,%wk72;+3:;WK3$qsX:>E^/ZweYT;RblK0JdSk8esv`hTfqY}Viy7');
+define('NONCE_KEY',        '4{M|s|/g#yq6tQX,Nmu+R(*3$}0gLquw15X5y7>A2JFU_Hjt|Fq&E*@28(BP~U|L');
+define('AUTH_SALT',        'c5Y^KQf?}h{Od |z:MV05?QbWg}e[yg`?}^H^eM8C}E|b0m*|Pq*)ceaN|JLPbl?');
+define('SECURE_AUTH_SALT', '2NT+L<#)CyAdrLOv9heBbG8B:9e^c2-G+|.OC7adHiz:U#gE&WExghJ_cKchONvA');
+define('LOGGED_IN_SALT',   'Qkk+<Tf[;|sfGF(G?L*t]_z!+^V>HKzimwZU0M&pBN1+S.gEH+v*JxS;+Xb_-W>t');
+define('NONCE_SALT',       '*_^*gG_>`t0a..iW-PE!fo6mH*g=<RknOA,+Vr]OL7]wDn<&iEqE@+i-3pVi$u92');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', true);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/themes/wen-theme/footer-old.php
@@ -0,0 +1,136 @@
+<?php // FOOTER PHP ?>
+			<div class="gold-line hidden" data-transition-delay="2" style="background-image: url(<?php echo get_template_directory_uri(); ?>/library/images/gold-border-bottom.png);"></div>
+
+			<footer class="footer hidden" data-transition-delay="3" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+
+				<div id="inner-footer" class="cf">
+
+					<ul>
+						<li class="contact-item">
+							<!-- <div class="footer-icon" style="background-image: url(<?php echo get_template_directory_uri(); ?>/library/images/logo-icon.svg);"></div> -->
+							<a class="address">
+								<div class="footer-icon">
+									<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+										 viewBox="0 0 93.5 124.7" enable-background="new 0 0 93.5 124.7" xml:space="preserve">
+										<polygon fill="#fff" points="2.7,64 2.7,122.7 90.9,122.7 90.9,2 "/>
+									</svg>
+								</div>
+								<h4>Address</h4>
+								<span class="address">
+									<address>54 West 21st Street, RM 508<br>NYC, NY 10010</address>
+								</span>
+							</a>
+						</li>
+
+						<li class="contact-item">
+							<!-- <div class="footer-icon" style="background-image: url(<?php echo get_template_directory_uri(); ?>/library/images/logo-icon.svg);"></div> -->
+							<a class="email" href="mailto:reps@thefamilynyc.com">
+								<div class="footer-icon">
+									<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+										 viewBox="0 0 93.5 124.7" enable-background="new 0 0 93.5 124.7" xml:space="preserve">
+										<polygon fill="#fff" points="2.7,64 2.7,122.7 90.9,122.7 90.9,2 "/>
+									</svg>
+								</div>
+								<h4>Email</h4>
+								<span class="email">reps@thefamilynyc.com</span>
+							</a>
+						</li>
+
+						<li class="contact-item">
+							<!-- <div class="footer-icon" style="background-image: url(<?php echo get_template_directory_uri(); ?>/library/images/logo-icon.svg);"></div> -->
+							<a class="phone" href="tel:2124774248">
+								<div class="footer-icon">
+									<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+										 viewBox="0 0 93.5 124.7" enable-background="new 0 0 93.5 124.7" xml:space="preserve">
+										<polygon fill="#fff" points="2.7,64 2.7,122.7 90.9,122.7 90.9,2 "/>
+									</svg>
+								</div>
+								<h4>Phone</h4>
+								<span class="phone">T: 212 477 4248</span>
+							</a>
+						</li>
+					</ul>
+
+					<div class="social-container">
+						<a href="https://www.facebook.com/pages/The-Family-NYC/317279850808?fref=ts" target="_blank">
+							<!-- <div id="facebook" style="background-image: url(<?php echo get_template_directory_uri(); ?>/library/images/social/fb.svg);"></div> -->
+							<div id="facebook">
+								<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+									 viewBox="0 0 36.9 36.9" enable-background="new 0 0 36.9 36.9" xml:space="preserve">
+									<path class="circle" fill="#FFFFFF" d="M18.4,35.4c9.4,0,17-7.6,17-17c0-9.4-7.6-17-17-17c-9.4,0-17,7.6-17,17C1.4,27.8,9,35.4,18.4,35.4"/>
+									<path fill="#1D381F" d="M14.3,15.5h1.8v-1.7c0-0.8,0-1.9,0.6-2.6c0.6-0.8,1.4-1.3,2.7-1.3c2.2,0,3.2,0.3,3.2,0.3l-0.4,2.6
+									c0,0-0.7-0.2-1.4-0.2c-0.7,0-1.3,0.2-1.3,0.9v2h2.8L22,18.1h-2.6v8.8h-3.3v-8.8h-1.8V15.5z"/>
+								</svg>
+							</div>
+						</a>						
+						<a href="https://twitter.com/thefamilynyc" target="_blank">
+							<!-- <div id="twitter" style="background-image: url(<?php echo get_template_directory_uri(); ?>/library/images/social/twitter.svg);"></div> -->
+							<div id="twitter">
+								<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+									 viewBox="0 0 36.9 36.9" enable-background="new 0 0 36.9 36.9" xml:space="preserve">
+									<path class="circle" fill="#FFFFFF" d="M18.4,35.4c9.4,0,17-7.6,17-17c0-9.4-7.6-17-17-17c-9.4,0-17,7.6-17,17C1.4,27.8,9,35.4,18.4,35.4"/>
+									<path fill="#1D381F" d="M27,14c-0.6,0.3-1.3,0.5-2,0.6c0.7-0.4,1.3-1.1,1.5-1.9c-0.7,0.4-1.4,0.7-2.2,0.8c-0.6-0.7-1.5-1.1-2.6-1.1
+									c-1.9,0-3.5,1.6-3.5,3.5c0,0.3,0,0.5,0.1,0.8c-2.9-0.1-5.5-1.5-7.2-3.7c-0.3,0.5-0.5,1.1-0.5,1.8c0,1.2,0.6,2.3,1.6,2.9
+									c-0.6,0-1.1-0.2-1.6-0.4v0c0,1.7,1.2,3.1,2.8,3.4c-0.3,0.1-0.6,0.1-0.9,0.1c-0.2,0-0.4,0-0.7-0.1c0.4,1.4,1.7,2.4,3.3,2.4
+									c-1.2,0.9-2.7,1.5-4.3,1.5c-0.3,0-0.6,0-0.8,0c1.5,1,3.4,1.6,5.4,1.6c6.4,0,9.9-5.3,9.9-9.9c0-0.2,0-0.3,0-0.5
+									C25.9,15.3,26.5,14.7,27,14"/>
+								</svg>
+							</div>
+						</a>
+						<a href="https://instagram.com" target="blank">
+							<!-- <div id="instagram" style="background-image: url(<?php echo get_template_directory_uri(); ?>/library/images/social/instagram.svg);"></div> -->
+							<div id="instagram">
+								<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+									 viewBox="0 0 36.9 36.9" enable-background="new 0 0 36.9 36.9" xml:space="preserve">
+									<path class="circle" fill="#FFFFFF" d="M18.4,35.4c9.4,0,17-7.6,17-17c0-9.4-7.6-17-17-17c-9.4,0-17,7.6-17,17C1.4,27.8,9,35.4,18.4,35.4"/>
+									<g class="circle">
+										<g>
+											<path fill="#1D381F" d="M23.2,10.2h-8.6V14h-0.7v-3.8h0c-0.2,0-0.4,0-0.5,0V14h-0.7v-3.5c-0.2,0.1-0.4,0.2-0.6,0.3V14h-0.7v-2.7
+												c-0.6,0.6-1,1.5-1,2.5v1.1h5.3c0.8-0.6,1.7-1,2.8-1c1.1,0,2,0.4,2.8,1h5.3v-1.1C26.6,11.8,25.1,10.2,23.2,10.2z M25.2,13.5
+												c0,0.2-0.2,0.4-0.4,0.4h-1.7c-0.2,0-0.4-0.2-0.4-0.4v-1.7c0-0.2,0.2-0.4,0.4-0.4h1.7c0.2,0,0.4,0.2,0.4,0.4V13.5z"/>
+											<path fill="#1D381F" d="M23,18.4c0,2.5-2,4.5-4.5,4.5c-2.5,0-4.5-2-4.5-4.5c0-1.1,0.4-2.1,1-2.9h-4.7V23c0,1.9,1.6,3.5,3.5,3.5
+												h9.3c1.9,0,3.5-1.6,3.5-3.5v-7.5H22C22.6,16.3,23,17.3,23,18.4z"/>
+										</g>
+										<path fill="#1D381F" d="M18.5,22.1c-2.1,0-3.8-1.7-3.8-3.8c0-2.1,1.7-3.8,3.8-3.8c2.1,0,3.8,1.7,3.8,3.8
+											C22.3,20.4,20.6,22.1,18.5,22.1z M18.5,15.1c-1.8,0-3.2,1.4-3.2,3.2c0,1.8,1.4,3.2,3.2,3.2c1.8,0,3.2-1.4,3.2-3.2
+											C21.7,16.6,20.3,15.1,18.5,15.1z"/>
+									</g>
+								</svg>
+							</div>
+						</a>
+					</div>
+
+					<div id="footer-logo" style="background-image: url(<?php echo get_template_directory_uri(); ?>/library/images/footer_logo.svg);"></div>
+					<div class="footer-credits">
+						<p>Branding by The Second Column.<span>|</span></p>
+						<p>&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.<span>|</span></p>
+						<p>Website by ClickFire Media.</p>
+					</div>
+
+				</div>
+
+			</footer>
+
+		</div>
+
+		<?php // all js scripts are loaded in library/bones.php ?>
+		<?php wp_footer(); ?>
+		<?php
+            // CONDITIONALLY LOAD OUTLINE SCRIPT
+            $server = $_SERVER['REMOTE_ADDR'];
+            // IF SERVER IS LOCAL, ADD OUTLINE BUTTON
+            if($server == '127.0.0.1') {
+                echo "<script>
+                    jQuery('head').append('<style>#outline {position:fixed;z-index:1000;bottom:50px;right:50px;} .outlines {outline:1px solid rgba(255, 0, 0, 0.3);}</style>');
+                    jQuery('body').append('<input id=\"outline\" type=\"button\">');
+
+                    jQuery('#outline').click(function() {
+                        jQuery('*').toggleClass('outlines');
+                   });
+                </script>";
+            }
+        ?>
+
+	</body>
+
+</html> <!-- end of site. what a ride! -->
