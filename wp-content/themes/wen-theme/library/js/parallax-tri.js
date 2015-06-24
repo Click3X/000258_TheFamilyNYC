9dc0c5fd0e02d2a61cd969cc005a39e568aec7d0
Charlie Davis <charliekuldip@gmail.com>
2015-06-24 16:48:15 -0400

    Worked on Parallax Tri js, added more specfic coords and classnames in js init array

--- wp-content/themes/wen-theme/library/js/parallax-tri.js
+++ wp-content/themes/wen-theme/library/js/parallax-tri.js
@@ -3,32 +3,35 @@ jQuery(document).ready(function($) {
 	console.log('Het from parallax tri');
 
 	var colors = ["gold-foil","green-tri","stripe-tri"];
+	// W, H, X, Y, color, up
 	var coords = [
-		[992, 250],
-		[284, -30],
-		[49, 324],
-		[830, -20],
-		[354, 444],
-		[265, 323],
-		[197, 718],
-		[1311, 206],
-		[-22, 660],
-		[1123, 822],
-		[228, 43],
-		[1344, 810],
-		[225, 722],
-		[1207, 573],
-		[1098, 39],
-		[1289, 535],
-		[64, 908],
-		[960, 615]
+		[207, 168, 1160, 250, "gold-foil", "left"],
+		[222, 184, 284, 192, "stripe-tri", "left"],
+		[162, 139, 188, 324, "stripe-tri", "half"],
+		[136, 110, 830, -20, "gold-foli", "reflect"],
+		[107, 88, 345, 444, "stripe-tri", "reflect"],
+		[167, 135, 265, 490, "gold-foil", "reflect"],
+		[109, 90, 306, 808, "stripe-tri", "reflect"],
+		[82, 124, 1311, 288, "green-tri", "up"],
+		[82, 124, 102, 660, "green-tri", "down"],
+		[55, 83, 1206, 822, "green-tri", "down"],
+		[73, 110, 228, 43, "green-tri", "right"],
+		[126, 102, 1385, 710, "gold-foil", "left"],
+		[111, 90, 225, 722, "gold-foli", "reflect"],
+		[133, 110, 1207, 573, "stripe-tri", "right"],
+		[133, 110, 1098, 38, "stripe-tri", "right"],
+		[73, 110, 1289, 535, "green-tri", "left"],
+		[83, 69, 64, 908, "stripe-tri", "back"],
+		[111, 91, 1071, 706, "stripe-tri", "reflect"]
 	];
+
 
 	// ADD TWICE AS MANY COORDS
 	$.each(coords, function(i, elem) {
 		console.log(coords[i][0], coords[i][1]);
 
-		coords.push( [ coords[i][0], coords[i][1] + 700 ] );
+		// coords.push( [ coords[i][2], coords[i][3] + 300 ] );
+		coords.push( [ coords[i][0], coords[i][1], coords[i][2], coords[i][3] , coords[i][4], coords[i][5] ] );
 	});
 
 	// CHECK COORDS
@@ -51,18 +54,19 @@ jQuery(document).ready(function($) {
 	var blobs = [];
 
 	function blob(count) {
-		console.log('This is coords: ' + coords[count][0]/1400 );
+		console.log('This is coords: ' + coords[count][2]/1400 );
 
 		this.posZ = getRandomInt(1, 150) - 100;
-		this.element = $('<div/>', { 'class':'tri'});
-		this.element.addClass(colors[Math.floor(Math.random() * colors.length)]); //random bg color
-		this.element.css('z-index', this.posZ);
+		// this.element = $('<div/>', { 'class':'tri'});
+		this.element = $('<div/>', { 'class':'tri '+ coords[count][4]});
+		// this.element.addClass(colors[Math.floor(Math.random() * colors.length)]); //random bg color
+		// this.element.css('z-index', this.posZ);
 		// this.element.css('left', getRandomInt(0,120)-10 + '%');
 		// this.element.css('left', (coords[Math.floor(Math.random() * coords.length)][0]/1400) * 100 + '%');
-		this.element.css('left', (coords[count][0]/1400) * 100 + '%');
+		this.element.css('left', (coords[count][2]/1400) * 100 + '%');
 		// this.startTop = getRandomInt(0,120)-10;
 		// this.startTop = (coords[Math.floor(Math.random() * coords.length)][1]/700) * 100;
-		this.startTop = (coords[count][1]/700) * 100;
+		this.startTop = (coords[count][3]/1000) * 100;
 		this.element.css('top', this.startTop + '%');
 		if (this.posZ > 0)
 			this.scale = (1 + scalemod * this.posZ / 100);
